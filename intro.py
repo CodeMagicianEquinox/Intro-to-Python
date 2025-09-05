c7741beb741ba717c2cc8977dca79e32335c9d27
CodeMagicianEquinox <timterrance.tech@gmail.com>
2025-09-04 20:37:27 -0500

    Added MiniChallenge1.md and mc1.py, modified intro.py

--- intro.py
+++ intro.py
@@ -1,1 +1,26 @@
 print("Hello World")
+print(2)
+print(5+3)
+print(True)
+
+
+#This is a comment...
+
+'''
+Multi line comment
+'''
+
+#Creating variables
+name = "Luis"
+age = 24
+last_name = "Terrance"
+found = False
+
+print(name)
+print(last_name)
+
+#Concatenation
+#Casting -> convert data type to another one (string to numbers)
+print("My name is: " + name + " and I'm " + str(age) + " years old")
+print("Did he show up to class? " + str(found))
+print(100 + int(another_age))
---
+++ mc1.py
@@ -0,0 +1,10 @@
+#Declare the vars as so
+name = "vinny"
+age = 100
+last_name = "bagofdonuts"
+found = True
+#Concate/Cast them as so
+
+print("My name is: " + name + " " + last_name + " and I'm " + str(age) + " years old")
+print("Does he know Gandolf the grey? " + str(found))
+print("Gandolf is also 100+ years old. They are friends.")
